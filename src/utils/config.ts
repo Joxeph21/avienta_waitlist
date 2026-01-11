3b355757c023113a55dea47b8a08482fae1a6c89
Joxeph21 <codewithjoxeph@gmail.com>
2026-01-11 01:46:00 +0100

    fetat: made changes to include s ocails

---
+++ src/utils/config.ts
@@ -0,0 +1,8 @@
+export const SOCIAL_LINKS = [
+  { name: "Instagram", url: "https://www.instagram.com/officialavienta" },
+  { name: "X", url: "https://x.com/officialavienta" },
+//   { name: "LinkedIn", url: "https://www.linkedin.com/company/avienta-store/" },
+  { name: "Facebook", url: "https://web.facebook.com/profile.php?id=61586371139627" },
+];
+
+export const SUPPORT_EMAIL = "support@avienta.store";
